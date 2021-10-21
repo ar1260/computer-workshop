ab51b3ce0a0b1966b70e8e49b300f13313d7413f
ar1260 <91151315+ar1260@users.noreply.github.com>
2021-10-21 23:14:37 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "function.h"
+#include <locale.h>
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	EX_7();
+
+	return 0;
+}
